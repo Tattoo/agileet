052980776b81c31d0d3f82201197aa534350c10b
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-17 18:11:24 +0000

    done add event action-test -TKa

--- tests/pokkare/tests/AddEventActionTest.java
+++ tests/pokkare/tests/AddEventActionTest.java
@@ -1,15 +1,48 @@
 package pokkare.tests;
-import junit.framework.TestCase;
 
+import junit.framework.TestCase;
+import pokkare.action.AddEventAction;
+import java.util.HashMap;
+import java.util.Date;
 
 public class AddEventActionTest extends TestCase {
 
+	private AddEventAction action;
+	
 	protected void setUp() throws Exception {
-		super.setUp();
+		action = new AddEventAction();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		action = null;
 	}
-
+	
+	public void testGetHosts(){
+		assertEquals(action.getHosts().size(), 2);
+	}
+	public void testSetHosts(){
+		HashMap<Integer, String> test = new HashMap<Integer, String>();
+		test.put(0, "foofoo");
+		action.setHosts(test);
+		assertSame(action.getHosts().get(0), "foofoo");
+	}
+	public void testDescAccessors(){
+		action.setDesc("this is description");
+		assertSame(action.getDesc(), "this is description");
+	}
+	public void testTimeAccessors(){
+		Date time = new Date(2009,1,1);
+		action.setTime(time);
+		Date time2 = new Date(2009,1,1);
+		assertEquals(action.getTime(), time2);
+	}
+	public void testHostAccessors(){
+		action.setHost(123);
+		assertEquals(action.getHost(), new Integer(123));
+	}
+	public void testExecute(){
+		assertEquals(action.execute(), "success");
+		action.setTime(new Date(2009,1,1));
+		assertEquals(action.execute(), "index");
+	}
 }
