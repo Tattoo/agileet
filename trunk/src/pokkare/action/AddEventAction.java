a096fcd91d72c7dd8f7215afa876cbda9c683fa0
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-17 21:14:53 +0000

    completed two test cases for AddEventAction and AddRankingAction, fixed indentation in the actual files -TKa

--- trunk/src/pokkare/action/AddEventAction.java
+++ trunk/src/pokkare/action/AddEventAction.java
@@ -1,10 +1,8 @@
 package pokkare.action;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import pokkare.model.Player;
 import pokkare.model.Games;
