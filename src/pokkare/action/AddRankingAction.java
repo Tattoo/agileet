35c2a6f5c790859ea3b98558ab378b64864f2209
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-17 21:14:53 +0000

    completed two test cases for AddEventAction and AddRankingAction, fixed indentation in the actual files -TKa

--- src/pokkare/action/AddRankingAction.java
+++ src/pokkare/action/AddRankingAction.java
@@ -14,98 +14,100 @@ import pokkare.model.Score;
 import pokkare.service.EventService;
 
 public class AddRankingAction implements ParameterAware {
-	
+
 	private HashMap<Integer, String> gamesMap = new HashMap<Integer, String>();
 	EventService event = new EventService();
 	private static Integer chosenGame = -1;
 	private ArrayList<Player> playerList = new ArrayList<Player>();
 	private Map parameters;
-	
+
 	public Map getParameters() {
 		return parameters;
 	}
-	
+
 	public void setParameters(Map parameters) {
 		this.parameters = parameters;
 	}
-	
+
 	public void setPlayerList(ArrayList<Player> playerList) {
 		this.playerList = playerList;
 	}
-	
+
 	public ArrayList<Player> getPlayerList() {
 		return playerList;
 	}
-	
+
 	public void setChosenGame(Integer chosenGame) {
 		this.chosenGame = chosenGame;
 	}
-	
+
 	public static Integer getChosenGame() {
 		return chosenGame;
 	}
-	
-	
+
+
 	public void setGamesMap(HashMap<Integer, String> gamesMap) {
 		this.gamesMap = gamesMap;
 	}
-	
+
 	public HashMap<Integer, String> getGamesMap() {
 		return gamesMap;
 	}
-	
+
 	public AddRankingAction() {
-		
+
 	}
-	
+
 	public String execute() {
 		playerList = (ArrayList<Player>)event.findPlayers();
-		
+
 		if (chosenGame == null || chosenGame < 0) {
-		
-		ArrayList<Games> a = (ArrayList<Games>)event.findGames();
-	
+
+			ArrayList<Games> a = (ArrayList<Games>)event.findGames();
+
 			for (int i = 0; i < a.size(); ++i) {
 				gamesMap.put(a.get(i).getId(), a.get(i).getGameDate() + " nro: " + a.get(i).getGameNumber());
 			}
 			System.out.println("success");
 			return "success";
 		}
-		
-		//tässä luupataan läpi parameters-mappi josta haetaan pelaaja, 
+
+		//tï¿½ssï¿½ luupataan lï¿½pi parameters-mappi josta haetaan pelaaja, 
 		//pelaajaID ja tallennetaan score jokaiselle mapin pelaajalle
-		
+
 		for (int i = 0; i < playerList.size(); ++i) {
-			
+
 			Player player = (Player)playerList.get(i);
 			String playerName = player.getName();
 			Integer playerId =player.getId();
-			
+
 			if (parameters.containsKey(playerName)) {
-				
+
 				Integer playerRank = -1;
-				
+
 				try { 
-				playerRank = Integer.parseInt(((String[])parameters.get(playerName))[0]);
-				} catch (Exception e) { e.printStackTrace(); throw new IllegalStateException("Convert mismatch"); }
-				
+					playerRank = Integer.parseInt(((String[])parameters.get(playerName))[0]);
+				} catch (Exception e) { 
+					e.printStackTrace(); 
+					throw new IllegalStateException("Convert mismatch"); 
+				}
+
 				Score score = new Score();
 				score.setGameScoreId(chosenGame);
 				score.setPlayerId(playerId);
 				score.setRank(playerRank);
-				
+
 				System.out.println("saving score");
 				event.saveScore(score);
 			}
 		}
-		
-		
+
+
 		//System.out.println(parameters.size() + " llss " + ((String[])parameters.get("Jones"))[0]);
 		System.out.println("index");
 		chosenGame = -1;
 		return "index";
-	
+
 	}
-	
+
 }
-	
--- tests/pokkare/tests/AddRankingActionTest.java
+++ tests/pokkare/tests/AddRankingActionTest.java
@@ -1,15 +1,91 @@
 package pokkare.tests;
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
+import pokkare.action.AddRankingAction;
+import pokkare.model.Player;
+import pokkare.model.Score;
+import pokkare.service.EventService;
 
 public class AddRankingActionTest extends TestCase {
 
+	private AddRankingAction ranking;
+	private HashMap<String, String[]> testparams;
+	private EventService event;
+	
 	protected void setUp() throws Exception {
-		super.setUp();
+		ranking = new AddRankingAction();
+		testparams = new HashMap<String, String[]>();
+		String[] testranking1 = {"1"};
+		String[] testrankin2 = {"2"};
+		testparams.put("jouns", testranking1);
+		testparams.put("tattoo", testrankin2);
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		ranking = null;
+		testparams = null;
+	}
+	
+	public void testPlayerListAccessors(){
+		ArrayList<Player> temp = new ArrayList<Player>();
+		Player p = new Player();
+		p.setId(999);
+		p.setName("Foo Foo");
+		temp.add(p);
+		ranking.setPlayerList(temp);
+		assertEquals(1, ranking.getPlayerList().size());
+		assertSame(p, ranking.getPlayerList().get(0));
+	}
+	
+	// suppressed warning about accessing static method un-static way 
+	@SuppressWarnings("static-access")
+	public void testGetChosenGame(){
+		assertEquals(ranking.getChosenGame(), new Integer(-1));
+	}
+	
+	// suppressed warning about accessing static method un-static way	
+	@SuppressWarnings("static-access")
+	public void testSetChosenGame(){
+		testGetChosenGame();
+		// save game so we can tear this case down
+		Integer game = ranking.getChosenGame();  
+		ranking.setChosenGame(new Integer(999));
+		assertEquals(new Integer(999), ranking.getChosenGame());
+		ranking.setChosenGame(game);
+		testGetChosenGame(); // test that tear down was successful
+	}
+	
+	public void testGamesListAccessors(){
+		assertEquals(new HashMap<Integer, String>(), ranking.getGamesMap());
+		HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+		hashmap.put(0, "Bar Bar");
+		ranking.setGamesMap(hashmap);
+		assertEquals(hashmap, ranking.getGamesMap());
+	}
+	
+	public void testExecute_ChosenGameLessThanZero(){
+		assertEquals(ranking.execute(), "success");
+	}
+	
+	public void testParametersAccessors(){
+		assertEquals(null, ranking.getParameters());
+		ranking.setParameters(testparams);
+		assertEquals(testparams, ranking.getParameters());
 	}
 
+	// TODO: this test case must be able to remove the entry from db
+	// 		before we can run it
+	/*
+	public void testExecute_rest(){
+		// execute tests we're depending on
+		testParametersAccessors();
+		ranking.setChosenGame(new Integer(999));
+		ranking.setParameters(testparams);
+		assertEquals("index", ranking.execute());
+	}
+	*/
 }
