179426efefe1aebff26977680fac780148c11d08
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-18 22:44:42 +0000

    unit tests for Player-model -TKa

--- trunk/tests/pokkare/tests/PlayerTest.java
+++ trunk/tests/pokkare/tests/PlayerTest.java
@@ -1,15 +1,29 @@
 package pokkare.tests;
 import junit.framework.TestCase;
 
+import pokkare.model.Player;
 
 public class PlayerTest extends TestCase {
-
+	Player testplayer;
 	protected void setUp() throws Exception {
-		super.setUp();
+		testplayer = new Player();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		testplayer = null;
+	}
+	
+	public void testIdAccessors(){
+		assertNull(testplayer.getId());
+		testplayer.setId(1);
+		assertEquals(new Integer(1), testplayer.getId());
+	}
+	
+	public void testNameAccessors(){
+		assertNull(testplayer.getName());
+		String name = "Foo B'ar Foo";
+		testplayer.setName(name);
+		assertEquals(name, testplayer.getName());
 	}
 
 }
