ad956b4d3e5859a551bab808ef3860eb96fe9ee1
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-18 22:39:13 +0000

    unit tests for IndexAction -TKa

--- tests/pokkare/tests/IndexActionTest.java
+++ tests/pokkare/tests/IndexActionTest.java
@@ -1,7 +1,36 @@
 package pokkare.tests;
 import junit.framework.TestCase;
 
+import pokkare.action.IndexAction;
+
+import java.util.ArrayList;
 
 public class IndexActionTest extends TestCase {
 
+	private IndexAction index;
+	
+	protected void setUp() throws Exception {
+		index = new IndexAction();
+	}
+	
+	protected void tearDown() throws Exception  {
+		index = null;
+	}
+	
+	public void testRankingAccessors(){
+		assertEquals(new ArrayList<String>(), index.getRanking());
+		ArrayList<String> ranking = new ArrayList<String>();
+		ranking.add("1");
+		ranking.add("2");
+		ranking.add("3");
+		index.setRanking(ranking);
+		assertEquals(3, index.getRanking().size());
+		for (int i = 0; i < ranking.size(); i++){
+			assertEquals(ranking.get(i), index.getRanking().get(i));
+		}
+	}
+	
+	public void testExecute(){
+		assertEquals("success", index.execute());
+	}
 }
