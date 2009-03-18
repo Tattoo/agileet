7620375f8e34024b200b6b69b9e5d6d09470b322
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-18 23:23:31 +0000

    removed unused imports from EventServiceTest unit tests for PointsService -TKa

--- trunk/tests/pokkare/tests/PointsServiceTest.java
+++ trunk/tests/pokkare/tests/PointsServiceTest.java
@@ -1,15 +1,48 @@
 package pokkare.tests;
+
+import org.hibernate.Session;
+
 import junit.framework.TestCase;
 
+import pokkare.model.Points;
+import pokkare.service.HibernateUtil;
+import pokkare.service.PointsService;
 
 public class PointsServiceTest extends TestCase {
 
+	private PointsService pointS;
+	private Session session;
+	private Points testpoints; 
+	
 	protected void setUp() throws Exception {
-		super.setUp();
+		testpoints = new Points();
+		testpoints.setPoints(999999999);
+		testpoints.setRank(999999999);
+		
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.save(testpoints);
+		session.getTransaction().commit();
+		session.close();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.delete(testpoints);
+		session.getTransaction().commit();
+		session.close();
+		session = null;
+		
+		testpoints = null;
 	}
 
+	public void testGetScore(){
+		/* PointsService must be initialized here so the stuff we're doing to 
+		 * db in set up and tear down is there for use
+		 */
+		pointS = new PointsService(); 
+		assertEquals(testpoints.getPoints(), pointS.getScore(testpoints.getRank()));
+		pointS = null;
+	}
 }
