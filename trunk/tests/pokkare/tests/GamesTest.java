099bad97d9713dd8c7e06101ff0228777ea06282
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-18 22:22:01 +0000

    unit tests for Games-model -TKa

--- trunk/tests/pokkare/tests/GamesTest.java
+++ trunk/tests/pokkare/tests/GamesTest.java
@@ -1,15 +1,54 @@
 package pokkare.tests;
 import junit.framework.TestCase;
 
+import pokkare.model.Games;
 
+import java.util.Date;
 public class GamesTest extends TestCase {
-
+	Games testgames;
 	protected void setUp() throws Exception {
-		super.setUp();
+		testgames = new Games();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		testgames = null;
 	}
 
+	public void testIdAccessors(){
+		assertNull(testgames.getId());
+		testgames.setId(1);
+		assertEquals(new Integer(1), testgames.getId());
+	}
+	
+	public void testDescriptionAccessors(){
+		assertNull(testgames.getDescription());
+		String desc = "this is a description.";
+		testgames.setDescription(desc);
+		assertEquals(desc, testgames.getDescription());
+	}
+	
+	public void testHostAccessors(){
+		assertNull(testgames.getHost());
+		testgames.setHost(1);
+		assertEquals(new Integer(1), testgames.getHost());
+	}
+	
+	public void testGameDateAccessors(){
+		assertNull(testgames.getGameDate());
+		Date d = new Date(new Long(0)); // this is epoch
+		testgames.setGameDate(d);
+		assertEquals(d, testgames.getGameDate());
+	}
+	
+	public void testGameNumberAccessors(){
+		assertNull(testgames.getGameNumber());
+		testgames.setGameNumber(1);
+		assertEquals(new Integer(1), testgames.getGameNumber());
+	}
+	
+	public void testGameScoreIdAccessors(){
+		assertNull(testgames.getGameScoreId());
+		testgames.setGameScoreId(1);
+		assertEquals(new Integer(1), testgames.getGameScoreId());
+	}
 }
