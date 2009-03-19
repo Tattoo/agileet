21f4849219cd1191286b6e209cfee7a643b6f35f
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-19 09:38:10 +0000

    unit tests for Score-model -TKa

--- tests/pokkare/tests/ScoreTest.java
+++ tests/pokkare/tests/ScoreTest.java
@@ -1,15 +1,40 @@
 package pokkare.tests;
 import junit.framework.TestCase;
-
+import pokkare.model.Score;
 
 public class ScoreTest extends TestCase {
 
+	private Score score;
+	
 	protected void setUp() throws Exception {
-		super.setUp();
+		score = new Score();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		score = null;
 	}
 
+	public void testIdAccessors(){
+		assertNull(score.getId());
+		score.setId(1);
+		assertEquals(new Integer(1), score.getId());
+	}
+	
+	public void testGameScoreIdAccessors(){
+		assertNull(score.getGameScoreId());
+		score.setGameScoreId(1);
+		assertEquals(new Integer(1), score.getGameScoreId());
+	}
+	
+	public void testPlayerIdAccessors(){
+		assertNull(score.getPlayerId());
+		score.setPlayerId(1);
+		assertEquals(new Integer(1), score.getPlayerId());
+	}
+	
+	public void testRankAccessors(){
+		assertNull(score.getRank());
+		score.setRank(1);
+		assertEquals(new Integer(1), score.getRank());
+	}
 }
