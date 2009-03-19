0497a11da3eb559644cda06383aec54082e64b25
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-19 09:04:36 +0000

    Slightly incomplete unit tests for PokkareGraphDrawer -TKa

--- trunk/tests/pokkare/tests/PokkareGraphDrawerTest.java
+++ trunk/tests/pokkare/tests/PokkareGraphDrawerTest.java
@@ -1,15 +1,46 @@
 package pokkare.tests;
+import java.io.FileOutputStream;
+
 import junit.framework.TestCase;
 
+import pokkare.service.PokkareGraphDrawer;
 
 public class PokkareGraphDrawerTest extends TestCase {
 
+	PokkareGraphDrawer graph;
+	
 	protected void setUp() throws Exception {
-		super.setUp();
+		graph = new PokkareGraphDrawer();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		graph = null;
 	}
-
+	
+	public void testMaxPointsAccessors(){
+		assertEquals(0, graph.getMaxPoints());
+		graph.setMaxPoints(1);
+		assertEquals(1, graph.getMaxPoints());
+	}
+	
+	public void testMultiplierAccessors(){
+		assertEquals(1, graph.getMultiplier());
+		graph.setMultiplier(2);
+		assertEquals(2, graph.getMultiplier());
+	}
+	
+/* This cannot be run until createImage() works on Linux
+ *  	
+	public void testCreateImage(){
+		try {
+			FileOutputStream f = new FileOutputStream("pokkaregraph.jpg");
+			assertNotNull(f);
+			graph.createImage(f);
+			f.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("testCreateImage() threw an exception");
+		}
+	}
+*/
 }
