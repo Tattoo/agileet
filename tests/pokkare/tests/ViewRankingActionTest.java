7bcd2501201c1a24f4c6d875776c1b54af18e6e2
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-19 09:53:18 +0000

    slightly incomplete unit tests for ViewRankingAction

--- tests/pokkare/tests/ViewRankingActionTest.java
+++ tests/pokkare/tests/ViewRankingActionTest.java
@@ -1,15 +1,56 @@
 package pokkare.tests;
 import junit.framework.TestCase;
-
+import pokkare.action.ViewRankingAction;
+import java.util.ArrayList;
 
 public class ViewRankingActionTest extends TestCase {
-
+	ViewRankingAction view;
 	protected void setUp() throws Exception {
-		super.setUp();
+		view = new ViewRankingAction();
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		view = null;
 	}
 
+	public void testSizeAccessors(){
+		assertNull(view.getSize());
+		String size = "11";
+		view.setSize(size);
+		assertEquals(size, view.getSize());
+	}
+	
+	public void testRankingAccessors(){
+		assertEquals(new ArrayList<String>(), view.getRanking());
+		ArrayList<String> testRanking = new ArrayList<String>();
+		testRanking.add("1");
+		testRanking.add("2");
+		testRanking.add("3");
+		view.setRanking(testRanking);
+		assertEquals(testRanking, view.getRanking());
+	}
+	/* TODO: When graph-drawing works, uncomment
+	 * 
+	 
+	public void testDrawPokkareGraph(){
+		try {
+			view.drawPokkareGraph(999);
+		} catch (Exception e){
+			e.printStackTrace();
+			fail("testDrawPokkareGraph() threw an exception");
+		}
+	}
+	
+	public void testExecute(){
+		String response = null;
+		try {
+			response = view.execute();
+		} catch (Exception e){
+			e.printStackTrace();
+			fail("testExecute() threw an exception");
+		} finally {
+			assertEquals("success", response);
+		}
+	}
+	*/
 }
