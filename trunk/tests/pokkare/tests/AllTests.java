63e1c2dfc215ee15dc9d67eb077e2e7bef42ec19
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-19 10:39:10 +0000

    re-arranged how test cases are run -TKa

--- trunk/tests/pokkare/tests/AllTests.java
+++ trunk/tests/pokkare/tests/AllTests.java
@@ -7,20 +7,26 @@ public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for pokkare.tests");
-		//$JUnit-BEGIN$
-		suite.addTestSuite(AddEventActionTest.class);
-		suite.addTestSuite(AddRankingActionTest.class);
+
+		//model tests
 		suite.addTestSuite(GamesTest.class);
 		suite.addTestSuite(PlayerTest.class);
-		suite.addTestSuite(ViewRankingActionTest.class);
-		suite.addTestSuite(ScoreServiceTest.class);
-		suite.addTestSuite(PokkareGraphDrawerTest.class);
-		suite.addTestSuite(PointsServiceTest.class);
+		suite.addTestSuite(ScoreTest.class);		
 		suite.addTestSuite(PointsTest.class);
-		suite.addTestSuite(IndexActionTest.class);
-		suite.addTestSuite(ScoreTest.class);
+		
+		//service tests
+		
+		suite.addTestSuite(PointsServiceTest.class); // MUST BE BEFORE PokkareGraphDrawerTest
 		suite.addTestSuite(EventServiceTest.class);
-		//$JUnit-END$
+		suite.addTestSuite(ScoreServiceTest.class);
+		suite.addTestSuite(PokkareGraphDrawerTest.class); // MUST BE AFTER PointsServiceTest
+				
+		// action tests
+		suite.addTestSuite(AddEventActionTest.class);
+		suite.addTestSuite(AddRankingActionTest.class);
+		suite.addTestSuite(ViewRankingActionTest.class);
+		suite.addTestSuite(IndexActionTest.class);
+
 		return suite;
 	}
 
