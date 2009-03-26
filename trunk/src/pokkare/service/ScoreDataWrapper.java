df01e8e0bb7fccc5ff31d763f92c3ca4b5b12eae
nuubian <nuubian@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-26 16:05:39 +0000

    de-coupling related

--- trunk/src/pokkare/service/ScoreDataWrapper.java
+++ trunk/src/pokkare/service/ScoreDataWrapper.java
@@ -1,7 +1,9 @@
 package pokkare.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 import pokkare.model.Games;
@@ -14,10 +16,87 @@ import pokkare.model.Player;
 public class ScoreDataWrapper {
 	
 	List<ScoreData> scoreDatas = new ArrayList<ScoreData>();
+	int maxScore;
+	int numberOfGames;
 	
+	public int getMaxScore() {
+		return maxScore;
+	}
+
+	public void setMaxScore(int maxScore) {
+		this.maxScore = maxScore;
+	}
+
+	public int getNumberOfGames() {
+		return numberOfGames;
+	}
+
+	public void setNumberOfGames(int numberOfGames) {
+		this.numberOfGames = numberOfGames;
+	}
+
+	public void setScoreDatas(List<ScoreData> scoreDatas) {
+		this.scoreDatas = scoreDatas;
+	}
+
 	public List<ScoreData> getScoreDatas() {
 		return scoreDatas;
 	}
+	
+	public ScoreDataWrapper() {
+		ScoreData scoreData = null;
+		
+		EventService event = new EventService();
+		
+		//get games and players
+		ArrayList<Games> games = (ArrayList<Games>)event.findGames();
+		ArrayList<Player> players = (ArrayList<Player>)event.findPlayers();
+
+		//auxiliary map to aid in keeping score of players' total scores
+		//<player id, player score>
+		Map<Integer, Integer> playerScores = new HashMap<Integer, Integer>();
+
+		Integer maxScore = 0;
+		
+		//loop players
+		for (int i = 0; i < players.size(); ++i) {
+			
+			//get score datas list
+			List<ScoreData> scoreDatas = this.getScoreDatas();
+			
+			Player player = players.get(i);
+			
+			//loop games
+			for (int j = 0; j < games.size(); ++j) {
+				Games game = games.get(j);
+				//get scores
+				Integer score = event.findScoreForGameAndPlayer(game.getId(), player.getId());
+				//find and update cumulative score for this player
+				if (playerScores.containsKey(player.getId())) {
+					score = score + playerScores.get(player.getId()).intValue();
+					playerScores.remove(player.getId());
+					playerScores.put(player.getId(), score);
+				}
+				//if none, initialize for this player
+				else {
+					playerScores.put(player.getId(), score);
+				}
+
+				if (score > maxScore) {
+					maxScore = score;
+				}
+				
+				//add new data object
+				scoreData = this.new ScoreData(player, game, score.intValue());
+				scoreDatas.add(scoreData);
+			}
+		}
+		
+		this.setMaxScore(maxScore);
+		this.setNumberOfGames(games.size());
+		
+	}
+	
 
 	/*
 	 * Data object of score data. Includes date of game, number of game and score fields. 
