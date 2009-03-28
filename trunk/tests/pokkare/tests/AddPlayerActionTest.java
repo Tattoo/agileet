d21a1df35c1795fe74a09dc3486ef4ca600199f4
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-28 11:28:42 +0000

    added unit tests for AddPlayerAction, added these to test suite -TKa

--- trunk/tests/pokkare/tests/AddPlayerActionTest.java
+++ trunk/tests/pokkare/tests/AddPlayerActionTest.java
@@ -1,5 +1,93 @@
+package pokkare.tests;
 import junit.framework.TestCase;
+import pokkare.action.AddPlayerAction;
+import pokkare.model.Player;
+import pokkare.service.EventService;
+import pokkare.service.HibernateUtil;
+
+import java.util.HashMap;
+
+import org.hibernate.Session;
 
 public class AddPlayerActionTest extends TestCase {
+
+	private AddPlayerAction action;
+
+	protected void setUp() throws Exception {
+		action = new AddPlayerAction();
+	}
 	
+	protected void tearDown() throws Exception {
+		action = null;
+	}
+	
+	public void testParametersAccessors(){
+		assertNull(action.getParameters());
+
+		HashMap<String, String[]> param = new HashMap<String, String[]>();
+		String[] value = {"bar"};
+		String key = "foo";
+		param.put(key, value);
+		
+		action.setParameters(param);
+		
+		assertEquals(1, action.getParameters().size());
+		assertTrue(action.getParameters().containsKey(key));
+		assertTrue(action.getParameters().containsValue(value));
+		assertTrue(action.getParameters().get(key).equals(value));
+		assertEquals("bar", ((String[])action.getParameters().get(key))[0]);
+		
+		action.setParameters(null); // cleanup after
+	}
+	
+	public void testExecute(){
+		try{
+			action.execute();
+			fail("execute() should've failed with NullPointerException in AddPlayerActionTest");
+		} catch (NullPointerException e){ // things okay, put rest of the test here inside the catch
+
+			HashMap<String, String[]> param = new HashMap<String, String[]>();
+			String playerName = "Rafael Tommasi";
+			String[] value = {playerName};
+			param.put("add_player_name", value);
+			
+			action.setParameters(param);
+			
+			assertEquals("success", action.execute());
+			assertEquals("name_already_exists", action.execute());
+			
+			assertTrue(deleteTestData(playerName));
+			
+			value[0] = "";
+			param.put("add_player_name", value);
+			try {
+				action.execute();
+				fail("execute() should've failed with IllegalArgumentException in AddPlayerActionTest");
+			} catch (IllegalArgumentException ex){ 
+				// TODO: put the rest of the test stuff here inside the catch
+			}
+		}
+	}
+	private boolean deleteTestData(String playerName){
+		// delete stuff that was put in db during this test case
+		EventService event = new EventService();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			for (Player p : event.findPlayers()){
+				if (p.getName().compareTo(playerName) == 0){
+					session.delete(p);
+				}
+			}
+			session.getTransaction().commit();
+			session.close();
+			return true;
+		} catch (Exception e){
+			e.printStackTrace();
+			if (session != null && session.isOpen()){
+				session.close();
+			}
+			return false;
+		}
+	}
 }
