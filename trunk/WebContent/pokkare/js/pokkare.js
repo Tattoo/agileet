81c364a9dbddc041cb069a9759a5e58029072ee2
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-03-30 13:42:31 +0000

    implemented ui for adding new player. tested to work on linux. -TKa

--- trunk/WebContent/pokkare/js/pokkare.js
+++ trunk/WebContent/pokkare/js/pokkare.js
@@ -1,21 +1,32 @@
-			(function(){
-				$(document).ready(function(){
-					$("#index").corner("30px");
-					$("#menu").corner("4px sharp");
-					$("#menu li:last").css("margin-right","0em");
-					$("#menu li a").hover(function(){
-						$(this).css("border-bottom", "1px dashed");
-					}, function(){
-						$(this).css("border-bottom", "none");
-					});
-					if(String(window.location).indexOf("addevent") != -1){ // check that we are in right url
-						$("#time").datepicker({
-							showOn: 'button', 
-							buttonImage: '/pokkare/pokkare/images/datepicker.gif', 
-							buttonImageOnly: true,
-							firstDay: 1,
-							appendText: ' <small>(mm-dd-yyyy)</small>'
-						});
-					}
-				});
-			})();
+(function(){
+	$(document).ready(function(){
+		$("#index").corner("30px");
+		$("#menu").corner("4px sharp");
+		$("#menu li:last").css("margin-right","0em");
+
+		$("#menu li a").hover(function(){
+			$(this).css("border-bottom", "1px dashed");
+		}, function(){
+			$(this).css("border-bottom", "none");
+		});
+		
+		if(String(window.location).indexOf("addevent") != -1){ // check that we are in right url
+			$("#time").datepicker({
+				showOn: 'button', 
+				buttonImage: '/pokkare/pokkare/images/datepicker.gif', 
+				buttonImageOnly: true,
+				firstDay: 1,
+				appendText: ' <small>(mm-dd-yyyy)</small>'
+			});
+		}
+		
+		if (String(window.location).indexOf("manageplayers") != -1){ // check that we are in the right url
+			$("#add_players_form").submit(function(event){
+				if ($("#add_player_name").attr("value") == "" ){
+					alert("Pelaajan nimi ei saisi olla tyhjä");
+					event.preventDefault();
+				}
+			});
+		}
+	});
+})();
--- trunk/WebContent/pokkare/manageplayers.jsp
+++ trunk/WebContent/pokkare/manageplayers.jsp
@@ -1,1 +1,14 @@
-testi
+<jsp:include page="include/header.jsp"></jsp:include>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+
+<h1>Lis‰‰ pelaajia</h1>
+
+<form id="add_players_form" action="<s:url value='pokkare/addplayer.action'/>" method="post" >
+	<label for="add_player_name">Uuden pelaajan nimi:</label>
+	<input type="text" id="add_player_name" name="add_player_name" value="" /> <br />
+	<input type="submit" value="Lis‰‰ pelaaja">
+</form>
+
+<h1>Poista pelaajia</h1>
+
+<jsp:include page="include/footer.jsp"></jsp:include>
