09e0ee1d0f42e583b2edf973ecab7650ed4b5b57
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-04-09 12:43:04 +0000

    commented stuff on success and error messages -TKa

--- trunk/src/pokkare/service/ActionMessages.java
+++ trunk/src/pokkare/service/ActionMessages.java
@@ -1,6 +1,20 @@
 package pokkare.service;
 
-
+/* The character sets are different in different platforms.
+ * Therefore it is better to keep message texts that are rendered in html anyway as
+ * per html convention suggests, eg: 
+ * 		"Š" -> "&auml;"
+ * 		"š" -> "&ouml;"
+ * 		"Œ" -> "&aring;"
+ * 		"€" -> "&Auml;"
+ * 		"…" -> "&Ouml;"
+ * 		"" -> "&Aring;"
+ * 		"<" -> "&gt;"
+ * 		">" -> "&lt;"
+ *		etc. etc.
+ *
+ * check more at: http://www.ascii.cl/htmlcodes.htm
+ */ 
 public class ActionMessages {
 	public static final String PLAYER_REACTIVATED = "Pelaaja aktivoitu.";
 	public static final String GAME_ADDED = "Peli lis&auml;tty.";
