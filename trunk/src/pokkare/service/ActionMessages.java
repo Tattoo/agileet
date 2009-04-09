26229eb011dd7afbd8243aed85e0631181ce9acb
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-04-09 12:36:57 +0000

    refactored error and success messages to adhere proper html convention -TKa

--- trunk/src/pokkare/service/ActionMessages.java
+++ trunk/src/pokkare/service/ActionMessages.java
@@ -3,8 +3,8 @@ package pokkare.service;
 
 public class ActionMessages {
 	public static final String PLAYER_REACTIVATED = "Pelaaja aktivoitu.";
-	public static final String GAME_ADDED = "Peli lisätty.";
-	public static final String PLAYER_ADDED = "Pelaaja lisätty.";
-	public static final String RANKING_ADDED = "Ranking lisätty.";
+	public static final String GAME_ADDED = "Peli lis&auml;tty.";
+	public static final String PLAYER_ADDED = "Pelaaja lis&auml;tty.";
+	public static final String RANKING_ADDED = "Ranking lis&auml;tty.";
 	public static final String PLAYER_DELETED = "Pelaaja poistettu.";
 }
