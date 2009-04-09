921a354376b3e61a40ff196e8773eb5b6953d822
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-04-09 12:36:57 +0000

    refactored error and success messages to adhere proper html convention -TKa

--- src/pokkare/service/ErrorMessages.java
+++ src/pokkare/service/ErrorMessages.java
@@ -3,9 +3,9 @@ package pokkare.service;
 
 
 public class ErrorMessages {
-	public final static String PLAYER_BY_THIS_NAME_NOT_FOUND = "Virhe: tämän nimistä pelaajaa ei löytynyt.";
-	public final static String DATE_NOT_PROCESSABLE = "Virhe: antamaasi päivämäärää ei voitu käsitellä.";
-	public final static String DUPLICATE_PLAYER_NAME = "Virhe: tämän niminen pelaaja on jo olemassa.";
-	public final static String INTERNAL_ERROR = "Virhe: tapahtui sisäinen virhe.";
+	public final static String PLAYER_BY_THIS_NAME_NOT_FOUND = "Virhe: t&auml;m&auml;n nimist&auml; pelaajaa ei l&ouml;ytynyt.";
+	public final static String DATE_NOT_PROCESSABLE = "Virhe: antamaasi p&auml;iv&auml;m&auml;&auml;r&auml;&auml; ei voitu k&auml;sitell&auml;.";
+	public final static String DUPLICATE_PLAYER_NAME = "Virhe: t&auml;m&auml;n niminen pelaaja on jo olemassa.";
+	public final static String INTERNAL_ERROR = "Virhe: tapahtui sis&auml;inen virhe.";
 	public final static String NO_PLAYERS_AVAILABLE = "Tietokannassa ei ole pelaajia.";
 }
