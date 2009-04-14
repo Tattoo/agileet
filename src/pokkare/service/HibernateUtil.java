a0676d685ac46607a5ab8a7ff8b93346a65511b0
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-04-14 22:01:39 +0000

    refactored HibernateUtil to catch the very specific exception -TKa

--- src/pokkare/service/HibernateUtil.java
+++ src/pokkare/service/HibernateUtil.java
@@ -1,5 +1,6 @@
 package pokkare.service;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -11,7 +12,7 @@ public class HibernateUtil {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
             sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
+        } catch (HibernateException ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
