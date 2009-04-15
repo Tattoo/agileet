23d5bf06e5003c12d8b5e323185807fb3feb6273
tatu.kairi <tatu.kairi@be3f3e42-0f01-11de-9917-5710f4ffc9c1>
2009-04-15 09:14:57 +0000

    refactored IndexAction and ViewRankingAction to be easier to test -TKa

--- trunk/src/pokkare/action/ViewRankingAction.java
+++ trunk/src/pokkare/action/ViewRankingAction.java
@@ -89,51 +89,6 @@ public class ViewRankingAction  {
 	}
 	
 	public String execute() {
-
-		ArrayList<Player> playerList = (ArrayList<Player>)event.findPlayers();
-
-		Integer[] scoreTable = new Integer[playerList.size()];
-		String[] nameTable = new String[playerList.size()];
-
-		for (int i = 0; i < playerList.size(); ++i) {
-			Integer playerId = playerList.get(i).getId();
-			String playerName = playerList.get(i).getName();
-			Integer cumulativeScore = 0;
-
-			ArrayList<Score> scores = (ArrayList<Score>)event.findScores(playerId);
-
-			for (int j = 0; j < scores.size(); ++j) {
-				Score score = scores.get(j);
-				Integer rank = score.getRank();
-				cumulativeScore = cumulativeScore + event.findScore(rank);
-			}
-
-			//ranking.put(cumulativeScore, playerName);
-			scoreTable[i] = cumulativeScore;
-			nameTable[i] = playerName;
-		}
-
-		//sorttaa nimet
-		for (int i = 0; i < scoreTable.length; ++i) {
-			for (int j = i + 1; j < scoreTable.length; ++j) {
-				if (scoreTable[i] < scoreTable[j]) {
-					Integer k = scoreTable[i];
-					scoreTable[i] = scoreTable[j];
-					scoreTable[j] = k;
-					String s = nameTable[i];
-					nameTable[i] = nameTable[j];
-					nameTable[j] = s;
-				}
-			}
-		}
-		
-
-		for (int i = 0; i < scoreTable.length; ++i) {
-			System.out.println(scoreTable[i] + nameTable[i]);
-			ranking.add(nameTable[i] + ": " + scoreTable[i]);
-		}
-		// TODO: remove everything above?
-
 		HashMap<String, HashMap<Integer, Integer>> data = new HashMap<String, HashMap<Integer, Integer>>();
 		HashMap<Integer, String> games = new HashMap<Integer, String>();
 		gamesList = new ArrayList<GameDataObject>();
